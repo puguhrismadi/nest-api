db6744cfdb3caa99948a7a4aad365e1d35dcd595
Puguh Rismadi Ismail <puguh.rismadi@gmail.com>
2023-09-22 14:35:10 +0700

    delete clean all file

--- dist/user/service/user/user.service.d.ts
+++
@@ -1,6 +0,0 @@
-import { UserDto } from 'src/user/dto/user.dto/user.dto';
-export declare class UserService {
-    users: UserDto[];
-    create(user: UserDto): UserDto;
-    findAll(): UserDto[];
-}
--- dist/user/user/user.controller.d.ts
+++
@@ -1,4 +0,0 @@
-export declare class UserController {
-    create(): string;
-    findAll(): string;
-}
